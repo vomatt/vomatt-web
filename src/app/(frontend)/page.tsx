7b7a0e1f249ac5ad7a0230448e753e179c41c35e
Hsiang Chin <chinssiang@gmail.com>
2026-03-13 21:44:44 +0800

    feat: update home page to use getPolls endpoint module

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,11 @@
-import { getPollsData } from '@/app/api/get-polls/getPollsData';
+import { getPolls } from '@/lib/api/endpoints/polls';
 import { LoginPrompt } from '@/components/LoginPrompt';
 import { getUserSession } from '@/data/auth';
 
 import { PollFeedList } from './_components/PollFeedList';
 
 export default async function Page() {
-	const [user, data] = await Promise.all([getUserSession(), getPollsData()]);
+	const [user, data] = await Promise.all([getUserSession(), getPolls()]);
 
 	return (
 		<div className="px-contain flex justify-center gap-10">
