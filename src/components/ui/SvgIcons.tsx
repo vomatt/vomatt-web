b8d4a7ecf3b477f01a529dd949c4649a00778ac1
Hsiang Chin <chinssiang@gmail.com>
2026-03-30 22:02:02 +0800

    refactor: update UI component library to use radix-ui package and standardize styling conventions

--- src/components/ui/SvgIcons.tsx
+++ src/components/ui/SvgIcons.tsx
@@ -573,3 +573,21 @@ export function InformationCircleOutline(props: IconProps) {
 		</svg>
 	);
 }
+
+export function PanelLeftIcon(props: IconProps) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...props}
+		>
+			<rect x="3" y="3" rx="2"></rect>
+			<path d="M9 3v18"></path>
+		</svg>
+	);
+}
